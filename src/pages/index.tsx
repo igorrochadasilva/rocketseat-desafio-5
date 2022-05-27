2afd8fddb6f51d4cda4d87bed5bff5c3bbfccb1e
igorsilva <igor.silva@performait.com>
2022-05-27 16:08:15 -0300

    completed style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { FaCalendar, FaUser } from 'react-icons/fa';
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +25,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={styles.contentContainer}>
+      <div className={styles.logo}>
+        <img src="/images/Logo.svg" alt="Logo" />
+      </div>
+      <div className={styles.posts}>
+        <Link href="/posts/">
+          <a className={styles.post}>
+            <strong>O mundo mudou e agora há desafios</strong>
+            <p>ash uaejii aiefjaiej aeif aiefjaeifanefiai efaiefaiefiae fa </p>
+            <span>
+              <FaCalendar /> 18 Abr 2021
+              <FaUser /> Danilo Vieira
+            </span>
+          </a>
+        </Link>
+        <Link href="/posts/">
+          <a className={styles.post}>
+            <strong>O mundo mudou e agora há desafios</strong>
+            <p>ash uaejii aiefjaiej aeif aiefjaeifanefiai efaiefaiefiae fa </p>
+            <span>
+              <FaCalendar /> 18 Abr 2021
+              <FaUser /> Danilo Vieira
+            </span>
+          </a>
+        </Link>
+        <Link href="/posts/">
+          <a className={styles.post}>
+            <strong>O mundo mudou e agora há desafios</strong>
+            <p>ash uaejii aiefjaiej aeif aiefjaeifanefiai efaiefaiefiae fa </p>
+            <span>
+              <FaCalendar /> 18 Abr 2021
+              <FaUser /> Danilo Vieira
+            </span>
+          </a>
+        </Link>
+      </div>
+      <div className={styles.loading}>
+        <button>Carregar mais posts</button>
+      </div>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', { pageSize: 2 });
+  const postsPagination = {
+    results: postsResponse.results,
+    next_page: postsResponse.results_per_page,
+  };
 
-//   // TODO
-// };
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
